c7b53d5fe397ca3e7b999788be486950f957f62a
181dev <yuiwai.dev@gmail.com>
2014-11-27 13:47:56 +0900

    [WIP] Rewrite in vue.js

--- public/javascripts/blog.js
+++ public/javascripts/blog.js
@@ -1,95 +1,108 @@
-var teamlogApp = angular.module('teamlogApp', []);
+var request = window.superagent;
 
-function BlogCtrl($scope, $http, $location) {
-	$scope.postForm = false;
-	$scope.message = "";
+app = new Vue({
+	el:"#BlogCtrl",
+	data: {
+		postForm : false,
+		message:""
+	},
+	methods: {
+		update: function() {
+			var searchObject = $location.search();
+			var tag = searchObject.tag ? "?tag=" + searchObject.tag : "";
+			request.get("/blogs/" + tag).end(function (data) {
+				$scope.data = data.data;
+			});
 
-	$scope.showPostForm = function () {
-		$scope.postForm = !$scope.postForm;
-	}
-	$scope.$on('$locationChangeStart', function (next, current) {
-		update();
-	});
-	$scope.edit = function (item) {
-		$scope.postForm = true;
-		$scope.message = "編集中";
-		$scope.oid = item._id;
-		$scope.name = item.user;
-		$scope.title = item.title;
-		$scope.content = item.content;
-		$scope.tag = item.tag;
-	}
-	$scope.delete = function (id) {
-		$http.delete("/blogs/" + id).then(function (data) {
-			if (data.data[0]) {
-				update();
-				$scope.oid = "";
-				$scope.content = "";
-				$scope.title = "";
-				$scope.tag = "";
+			request.get("/tags").end(function (item) {
+				$scope.tags = item.data.tags;
+			});
+		},
+		showPostForm : function () {
+			$scope.postForm = !$scope.postForm;
+		},
+		edit : function (item) {
+			$scope.postForm = true;
+			$scope.message = "編集中";
+			$scope.oid = item._id;
+			$scope.name = item.user;
+			$scope.title = item.title;
+			$scope.content = item.content;
+			$scope.tag = item.tag;
+		},
+		delete : function (id) {
+			request.delete("/blogs/" + id).end(function (data) {
+				if (data.data[0]) {
+					update();
+					$scope.oid = "";
+					$scope.content = "";
+					$scope.title = "";
+					$scope.tag = "";
+				} else {
+					alert("削除出来ませんでした。" + data.data[1]);
+				}
+			});
+		},
+		sasuga : function (item) {
+			if (item.sasuga) {
+				item.sasuga = parseInt(item.sasuga, 10) + 1;
 			} else {
-				alert("削除出来ませんでした。" + data.data[1]);
+				item.sasuga = 1;
 			}
-		});
-	}
-	$scope.sasuga = function (item) {
-		if (item.sasuga) {
-			item.sasuga = parseInt(item.sasuga, 10) + 1;
-		} else {
-			item.sasuga = 1;
+
+			request.get("/sasuga/" + item._id).end(function (data) {});
+		},
+		post:function () {
+			var oid = $scope.oid;
+			var content = $scope.content;
+			var data = {
+				name: $scope.name,
+				content: $scope.content,
+				title: $scope.title,
+				tag: $scope.tag,
+				sasuga: 0
+			};
+			if (oid === undefined || oid === null || oid === "") {
+				request.post('/blogs/')
+					.send(data)
+					.end(function (err, res) {
+						if(err){
+							alert("更新エラー: " + err);
+							return;
+						}
+						update();
+						$scope.oid = "";
+						$scope.content = "";
+						$scope.title = "";
+						$scope.tag = "";
+
+					});
+			} else {
+				request.put('/blogs/' + oid)
+					.send(data)
+					.end(function (err, res) {
+						if(err){
+							alert("投稿エラー: " + err);
+							return;
+						}
+						update();
+						$scope.oid = "";
+						$scope.content = "";
+						$scope.title = "";
+						$scope.tag = "";
+					});
+			}
 		}
-
-		$http.get("/sasuga/" + item._id).then(function (data) {});
+	},
+	ready: function(){
+		/*
+		 $scope.$on('$locationChangeStart', function (next, current) {
+		 update();
+		 });
+		 */
+		this.update();
 	}
+});
 
-	function update() {
-		var searchObject = $location.search();
-		var tag = searchObject.tag ? "?tag=" + searchObject.tag : "";
-		$http.get("/blogs/" + tag).then(function (data) {
-			$scope.data = data.data;
-		});
 
-		$http.get("/tags").then(function (item) {
-			$scope.tags = item.data.tags;
-		});
 
-	}
-	update();
-	$scope.post = function () {
-		var oid = $scope.oid;
-		var content = $scope.content;
-		var data = {
-			name: $scope.name,
-			content: $scope.content,
-			title: $scope.title,
-			tag: $scope.tag,
-			sasuga: 0
-		};
-		if (oid === undefined || oid === null || oid === "") {
-			$http.post('/blogs/', data)
-				.success(function () {
-					update();
-					$scope.oid = "";
-					$scope.content = "";
-					$scope.title = "";
-					$scope.tag = "";
-
-				})
-				.error(function (data, status) {
-					alert("更新エラー: " + status);
-				});
-		} else {
-			$http.put('/blogs/' + oid, data)
-				.success(function () {
-					update();
-					$scope.oid = "";
-					$scope.content = "";
-					$scope.title = "";
-					$scope.tag = "";
-				})
-				.error(function (data, status) {
-					alert("投稿エラー: " + status);
-				});
-		}
-	};
-}
